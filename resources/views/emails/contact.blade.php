4c8b9e8909617c99f23fc5a96069be5017434741
gonzalesisherkyle <gonzalesisherkyle1@gmail.com>
2023-09-16 10:04:26 +0800

    change mailer to google smtp

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,2 +1,12 @@
-<h1>Contact from {{ $name }}</h1>
-<p>{{ $body }}</p><br />
+<!DOCTYPE html>
+<html>
+<head>
+    <title>You got mail!</title>
+</head>
+<body>
+    <h1>{{ $name }}</h1>
+    <p>{{ $body }}</p>
+     
+    <p>Thank you</p>
+</body>
+</html>
